592ff8604f680cf7a9897a8e06cc8597cf07a720
KarimKlaragym <karim.zuetar@elev.ga.klaragymnasium.se>
2024-02-12 10:47:29 +0100

    script

--- Unity 2d spel/Assets/ToolHit.cs
+++ Unity 2d spel/Assets/ToolHit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tool : MonoBehaviour
+public class ToolHit : MonoBehaviour
 {
     public virtual void Hit()
     {
