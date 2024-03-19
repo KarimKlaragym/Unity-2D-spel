8adeb3ad68fc69342cfe62dcaf53f0c19570ec79
KarimKlaragym <karim.zuetar@elev.ga.klaragymnasium.se>
2024-03-19 12:25:20 +0100

    inv

--- Unity 2d spel/Assets/Script/InventoryPanel.cs
+++ Unity 2d spel/Assets/Script/InventoryPanel.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class InventoryPanel : MonoBehaviour
 {
     [SerializeField] ItemContainer inventory;
+    [SerializeField] List<InventoryButton> buttons;
 
     private void start()
     {
+        SetIndex();
         Show();
+    }
+
+    private void SetIndex()
+    {
+        for (int i = 0;i < inventory.slots.Count;i++)
+        {
+            buttons[i].SetIndex(i);
+        }
     }
 
     private void Show()
     {
-
+        for(int i = 0; i< inventory.slots.Count; i++)
+        {
+            if (inventory.slots[i].item == null)
+            {
+                buttons[i].Clean(); ;
+            }
+            else
+            {
+                buttons[i].Set(inventory.slots[i]);
+            }
+        }
     }
 }
